dc75ea0032743391544df8ebe173182dfc026c46
Rohit <senr32245@gmail.com>
2024-08-21 14:24:55 +0530

    modified on booking_controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,7 @@
 class BookingsController < ApplicationController
+
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
 
   def index
