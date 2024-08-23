7538fdeeb2f9d107357b27e6c31e02cd5bcd9f5a
Rohit <senr32245@gmail.com>
2024-08-23 19:08:18 +0530

    modification

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :user
-  belongs_to :bus
+  belongs_to :bookable, polymorphic: true
+  
+  enum gender: { male: 0, female: 1 }
+  enum seat_type: { AC: 0, "Non&AC": 1,business: 3, first_class: 4}
 
   validates :name, :age, :gender, :date, :seat_type, presence: true
 end
--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,6 +1,6 @@
 class Bus < ApplicationRecord
   belongs_to :route
-  has_many :bookings
+  has_many :bookings, as: :bookable
 
   validates :name, :number, :total_seats, :bus_class, :price, presence: true
   validates :number, uniqueness: true
