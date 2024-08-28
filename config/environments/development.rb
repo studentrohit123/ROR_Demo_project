886f72f8b767160f83afa087db0a406235a419c1
Rohit <senr32245@gmail.com>
2024-08-28 19:20:50 +0530

    Add letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,4 +77,22 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "mydomain.net",
+    :user_name => "rsen@bestpeers.com",
+    :password => "eiys hady smql hbfo",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  } 
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
---
+++ config/initializers/smtp_settings.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "mydomain.net",
+    :user_name => "rsen@bestpeers.com",
+    :password => "eiys hady smql hbfo",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+}
