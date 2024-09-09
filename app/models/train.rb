4acafbb529a7ab5ca6b7393058c6e70fa30127a4
Rohit <senr32245@gmail.com>
2024-09-09 11:32:06 +0530

    test cases train model

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,5 +1,4 @@
 class Train < ApplicationRecord
   belongs_to :route
   has_many :bookings, as: :bookable
-  
 end
--- spec/model/bus_spec.rb
+++ spec/model/bus_spec.rb
@@ -13,5 +13,29 @@ RSpec.describe Bus, type: :model do
     context "Image for Bus" do
       it { should have_one_attached(:image) }
     end
+
+    context "Validation for Bus" do
+      it { should validate_presence_of(:name) }
+    end
+
+    context "Validation for Bus" do
+      it { should validate_presence_of(:number) }
+    end
+
+    context "Validation for Bus" do
+      it { should validate_presence_of(:total_seats) }
+    end
+
+    context "Validation for Bus" do
+      it { should validate_presence_of(:bus_class) }
+    end
+
+    context "Validation for Bus" do
+      it { should validate_presence_of(:price) }
+    end
+ 
+    context "Validation for Bus" do
+      it {  should validate_uniqueness_of(:number) }
+    end
   end
 end
